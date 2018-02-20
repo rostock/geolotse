df541c8ffbe784c6c4d590b78d223aa65df791b5
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2018-02-20 16:37:32 +0100

    geolocation; desktop and mobile layout fixtures; minor bugfixes

--- static/js/themes.js
+++ static/js/themes.js
@@ -7,9 +7,12 @@ if ($('#defining-container').data('mobile')) {
 } else {
   MOBILE = false;
 }
+FIRST_THEME = true;
 
 // initialise Leaflet
-proj4.defs('EPSG:25833', '+proj=utm +zone=33 +ellps=WGS84 +towgs84=0,0,0,0,0,0,1 +units=m +no_defs');        
+proj4.defs('EPSG:25833', '+proj=utm +zone=33 +ellps=WGS84 +towgs84=0,0,0,0,0,0,1 +units=m +no_defs');
+var defaultX = 12.0980385366347;
+var defaultY = 54.0929626081872;       
 var mapLayer = L.tileLayer('https://www.orka-mv.de/geodienste/orkamv/tiles/1.0.0/orkamv/GLOBAL_WEBMERCATOR/{z}/{x}/{y}.png', {
   minZoom: 17,
   attribution: '© Hanse- und Universitätsstadt Rostock (<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by/4.0/deed.de">CC BY 4.0</a>)',
@@ -22,6 +25,7 @@ var aerialLayer = L.tileLayer('https://geo.sv.rostock.de/geodienste/luftbild_mv-
 });
 var mapLayerTitle = $('#map-layer-title').data('translation');
 var aerialLayerTitle = $('#aerial-layer-title').data('translation');
+var locationControlTitle = $('#location-control-title').data('translation');
 var baseMaps = {};
 baseMaps[mapLayerTitle] = mapLayer;
 baseMaps[aerialLayerTitle] = aerialLayer;
@@ -32,16 +36,25 @@ var markers = new L.FeatureGroup();
 
 // functions
 
-function markerClick(e) {
+function centerMap(e) {
+  map.setView(e.latlng,5);
+}
+
+/*function markerClick(e) {
   if (typeof epsg != 'undefined') {
     target_x = x_orig;
     target_y = y_orig;
   } else {
-    var transformation = proj4('EPSG:25833', [x, y]);
+    var transformation = proj4('EPSG:25833', [defaultX, defaultY]);
     target_x = transformation[0];
     target_y = transformation[1];
   }
   window.open('https://www.geoport-hro.de/?poi[point]=' + target_x + ',' + target_y + '&poi[scale]=2133', '_blank');
+}*/
+
+function onLocationError(e) {
+  $('#location-error-modal').modal();
+  map.setView([defaultY, defaultX], 17);
 }
 
 function showMap(themeTitle) {
@@ -51,18 +64,37 @@ function showMap(themeTitle) {
   if (!map) {
     map = L.map('map', {
       layers: [mapLayer]
-    });
+    }).fitWorld();
     L.control.layers(baseMaps).addTo(map);
+    var locationControl = L.control.locate({
+    drawCircle: false,
+    drawMarker: false,
+    locateOptions: {
+        watch: true,
+        enableHighAccuracy: true
+    },
+    onLocationError: false,
+    setView: 'always',
+    strings: {
+        title: locationControlTitle
+    }
+    });
+    locationControl.addTo(map);
+    map.on('locationerror', onLocationError);
+    map.on('click', centerMap);
   }
   
-  var x = 12.0980385366347;
-  var y = 54.0929626081872;
+  if (MOBILE && FIRST_THEME) {
+    locationControl.start();
+  } else if (FIRST_THEME) {
+    map.setView([defaultY, defaultX], 17);
+  }
   
-  map.setView([y, x], 17);
-  markers.clearLayers();
-  var marker = L.marker([y, x], { title: themeTitle } ).on('click', markerClick);
-  markers.addLayer(marker);
-  map.addLayer(markers);
+  FIRST_THEME = false;
+  
+  /*markers.clearLayers();
+  var marker = L.marker([defaultY, defaultX], { title: themeTitle } ).on('click', markerClick).addTo(markers);
+  map.addLayer(markers);*/
 }
 
 
@@ -80,6 +112,8 @@ $(document).ready(function() {
   $('.slider').slick({
     dots: false,
     infinite: true,
+    focusOnSelect: true,
+    focusOnChange: true,
     slidesToScroll: 5,
     slidesToShow: 5,
     responsive: [
@@ -126,7 +160,7 @@ $(document).ready(function() {
   if (window.location.hash) {
     var anchor = window.location.hash;
     if ($(anchor).length && anchor.indexOf('theme-') !== -1) {
-      showMap($(anchor).data('map-theme-title'));
+      $(anchor).trigger('click');
     }
   }
 });
@@ -136,5 +170,9 @@ $(document).ready(function() {
 // other jQuery events
 
 $('.theme').click(function() {
-  showMap($(this).data('map-theme-title'));
+  if (!$(this).hasClass('active')) {
+    $('.theme').removeClass('active');
+    $(this).addClass('active');
+    showMap($(this).data('map-theme-title'), FIRST_THEME);
+  }
 });
